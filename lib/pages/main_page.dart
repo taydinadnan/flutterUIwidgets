18c7ce1718251357ab2867f2fdddb33ffc0f1c68
Adnan Turgay Aydin <taydinadnan@gmail.com>
2023-09-27 12:25:44 +0200

    Creating a bottom navigation bar and implementing the functionality

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_ui_widgets/models/article.dart';
-import 'package:flutter_ui_widgets/widgets/article_card.dart';
+import 'package:flutter_ui_widgets/pages/home_page.dart';
+import 'package:flutter_ui_widgets/pages/inbox_page.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
   const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  final List<Widget> _pages = [
+    const HomePage(),
+    const InboxPage(),
+  ];
+
+  int _selectedIndex = 0;
+
+  void _onTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,17 +32,23 @@ class MainPage extends StatelessWidget {
           width: 180,
         ),
       ),
-      body: ListView.builder(
-        itemCount: articles.length,
-        itemBuilder: (context, index) {
-          return ArticleCard(article: articles[index]);
-        },
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: [..._pages],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           print('Clicked');
         },
         child: const Icon(Icons.add),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onTapped,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.mail), label: 'Index')
+        ],
       ),
     );
   }
