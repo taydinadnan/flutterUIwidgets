f82bfebbdfb0f779a46ef4f8f52c813982d2e98d
Adnan Turgay Aydin <taydinadnan@gmail.com>
2023-09-27 12:30:32 +0200

    Adding messages model and displaying messages

--- lib/pages/inbox_page.dart
+++ lib/pages/inbox_page.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ui_widgets/models/message.dart';
 
 class InboxPage extends StatelessWidget {
-  const InboxPage({super.key});
+  const InboxPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView.builder(
+      itemCount: messages.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          onTap: () {},
+          leading: const CircleAvatar(),
+          title: Text(
+            messages[index].author,
+          ),
+          subtitle: Text(
+            messages[index].content,
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+          ),
+          trailing: Text(messages[index].sentTime),
+        );
+      },
+    );
   }
 }
